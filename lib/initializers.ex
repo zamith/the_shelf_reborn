f9bfe6a9dd29db1a30770cbf9edddcd1c95f13fc
Zamith <zamith.28@gmail.com>
2014-10-16 15:08:02 +0100

    Correctly configures rotors

--- lib/initializers.ex
+++ lib/initializers.ex
@@ -1,10 +1,14 @@
 import Rotor.BasicRotors
 import SassRotor
 
-output_path = "priv/static/assets/application.css"
-Rotor.watch :stylesheets, ["priv/assets/stylesheets/*.sass"], fn(_changed_files, all_files)->
-  read_files(all_files)
-  |> sass
-  |> concat
-  |> output_to(output_path)
+defmodule TheShelf.Initializers do
+  def start_rotors do
+    output_path = "priv/static/assets/application.css"
+    Rotor.watch :stylesheets, ["priv/assets/stylesheets/*.sass"], fn(_changed_files, all_files)->
+      read_files(all_files)
+      |> sass
+      |> concat
+      |> output_to(output_path)
+    end
+  end
 end
