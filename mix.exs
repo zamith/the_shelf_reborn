f90912ce35c2a12d08d6c40e682bd9afde37f98d
Zamith <zamith.28@gmail.com>
2014-10-19 16:16:16 -0500

    Improves deps management for multiple envs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TheShelf.Mixfile do
       version: "0.0.1",
       elixir: "~> 1.0.0-rc1",
       elixirc_paths: ["lib", "web", "test"],
-      deps: deps ]
+      deps: deps(Mix.env) ]
   end
 
   # Configuration for the OTP application
@@ -15,11 +15,16 @@ defmodule TheShelf.Mixfile do
       applications: apps(Mix.env) ]
   end
 
+  defp deps(:test) do
+    [
+      {:hound, path: "https://github.com/zamith/hound"},
+    ] ++ apps
+  end
+  defp deps(_), do: deps
   defp deps do
     [
       {:cowboy, "~> 1.0.0"},
       {:ecto, "~> 0.2.0"},
-      {:hound, path: "../hound"},
       {:phoenix, "0.4.1"},
       {:phoenix_haml, "~> 0.0.3"},
       {:postgrex, ">= 0.0.0"},
